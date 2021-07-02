0bf895d521dc89b65b638529a9219f5b231d6e90
William Young <wadyoung@outlook.com>
2021-07-02 16:14:22 +0100

    Added ability to dock many bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,15 +3,19 @@ require_relative "bike"
 class DockingStation
   
   attr_reader :bike
+
+  def initialize
+    @station = []
+  end
   
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @station.empty?
+    @station.pop
   end
 
   def dock(bike)
-    fail "Too many bikes docked." if @bike
-    @bike = bike
+    fail 'Too many bikes docked.' if @station.count >= 20
+    @station << bike
   end
 end
 
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,7 +1,7 @@
 require "docking_station"
 
 describe DockingStation do
- 
+   
   describe '#release_bike' do
     it 'raises an error when there are no bikes available' do
       expect {subject.release_bike}.to raise_error "No bikes available"
@@ -9,28 +9,32 @@ describe DockingStation do
   end 
 
   describe "#dock" do
-    it "It raises an error if more than 1 bike" do
-    subject.dock(Bike.new)
+    it "It raises an error if more than 20 bike" do
+    20.times { subject.dock(Bike.new) }
     expect {subject.dock(Bike.new)}.to raise_error "Too many bikes docked."
     end
   end
-  
+
   it "bike is working" do
     bike1 = Bike.new
     subject.dock(bike1)
     bike = subject.release_bike
     expect(bike.working?).to eq(true) 
   end
+  
   it { is_expected.to respond_to(:dock).with(1).argument }
   it { is_expected.to respond_to(:bike) }
+  
   it "docks something" do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+    expect(subject.dock(bike)).to eq [bike]
   end
+  
   it "returns docked bikes" do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bike).to eq nil # Test doesnt show anything
   end
-
 end
+
+
