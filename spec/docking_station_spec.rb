1be6c0e013cfd0ee8692b1c3ee6c964a63ccf647
William Young <wadyoung@outlook.com>
2021-09-28 14:45:01 +0100

    Added working bikes to docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,12 @@
 require 'docking_station'
 
+
 describe Docking_station do
   it 'Docking station responds to release bike method' do
     expect(Docking_station.new).to respond_to(:release_bike)
+  end
+  it 'Releases a bike' do
+    bike = subject.release_bike
+    expect(bike).to respond_to(:working?)
   end
 end  
