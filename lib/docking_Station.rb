06de928d8e50b5c80ebfa341846fbff671aac9c3
William Young <wadyoung@outlook.com>
2021-09-28 15:36:20 +0100

    Added instance variable storage

--- lib/docking_Station.rb
+++ lib/docking_Station.rb
@@ -1,7 +1,17 @@
 require_relative 'bike'
 
 class Docking_station
+
+  def initialize
+    @storage = []
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def dock(bike)
+    @bike = bike
+    @storage << @bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,21 @@ require 'docking_station'
 
 
 describe Docking_station do
+  let(:bike) { subject.release_bike }
+    
   it 'Docking station responds to release bike method' do
     expect(Docking_station.new).to respond_to(:release_bike)
   end
-  it 'Releases a bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  
+  describe "#release_bike" do
+    it 'Releases a bike' do
+      expect(bike).to be_working
+    end
   end
-end  
+    
+  describe "#dock" do
+    it 'Docks a bike' do
+      expect(subject.dock(bike)).to eq([bike])
+    end 
+  end 
+end
