ab336c8bf3481cf77b2b5a1f2b7d23fe6769385b
William Young <wadyoung@outlook.com>
2021-09-30 17:07:01 +0100

    Cleaned up  tests

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -2,11 +2,10 @@ require 'docking_station'
 
 describe Van do
   let(:broken_bike) { double(:bike, broken: true) }
-  let(:docking_station) { Docking_station.new } # How to mock this? Can't call .dock on double 
-  let(:garage) { Garage.new }
+  let(:docking_station) { double(:station, dock: broken_bike, storage: [broken_bike]) }
+  let(:garage) { double(:garage, garage_storage: []) }
   describe "#collect" do
     it 'Allows the van to collect broken bikes from stations' do
-      docking_station.dock(broken_bike)
       expect(subject.collect(docking_station)).to eq([])
       expect(subject.van_storage).to eq([broken_bike])
     end
@@ -14,7 +13,6 @@ describe Van do
 
   describe "#drop_off" do
     it 'Allows the van to drop off broken bikes at the garage' do
-      docking_station.dock(broken_bike)
       subject.collect(docking_station)
       expect(subject.drop_off(garage)).to eq([])
       expect(garage.garage_storage).to eq([broken_bike])
